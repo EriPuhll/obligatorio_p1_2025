d65626d6a16fb6173ad385588bffca85cec89607
EriPuhll <146786312+EriPuhll@users.noreply.github.com>
2025-05-05 14:34:39 -0300

    Create maquina.py

---
+++ scr/entities/maquina.py
@@ -0,0 +1,7 @@
+
+class Cliente:
+  def __init__(self, id: int, telefono: str, correo_electronico:str):
+    self.id = id
+    self.telefono = telefono
+    self.correo_electronico = correo_electronico
+    
