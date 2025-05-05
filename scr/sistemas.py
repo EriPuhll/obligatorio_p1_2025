285b821d7cc665a38a902c0a833c08bb336a7188
EriPuhll <146786312+EriPuhll@users.noreply.github.com>
2025-05-05 14:01:40 -0300

    Update sistemas.py

--- scr/sistemas.py
+++ scr/sistemas.py
@@ -1,1 +1,13 @@
+from entities.pieza import Pieza
+from entities.maquina import Maquina
+from entities.cliente import Cliente
+from entities.pedido import Pedido
+from entities.reposicion import Reposicion
 
+class Sistema:
+  def __init__(self):
+    self.piezas = Dict [ int, Pieza ] = {}
+    self.maquinas = Dict [ int, Maquina ] = {}
+    self.clientes = Dict [ int, Cliente ] ={}
+    self.pedidos = Dict [ int, Pedido ] = {}
+    self.reposiciones = List [ Reposicion ] = []
