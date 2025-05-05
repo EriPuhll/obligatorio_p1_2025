ce9b191905f287273bee90b09beb24c7bbee715a
EriPuhll <146786312+EriPuhll@users.noreply.github.com>
2025-05-05 14:16:32 -0300

    Create piezas.py

---
+++ scr/entities/piezas.py
@@ -0,0 +1,9 @@
+
+class Pieza: 
+  def __init__(self, codigo: int, descripcion: str, costo: float, lote: int, cantidad: int, reposicon):
+    self.codigo = codigo
+    self.descripcion = descripcion
+    self.costo = costo
+    self.lote = lote
+    self.cantidad = cantidad
+    #Ver que hacer con reposición
