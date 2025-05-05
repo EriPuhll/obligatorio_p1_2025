3280bcc9974b2a2b12c26c8357185f11de6c1901
eri_eri <146786312+EriPuhll@users.noreply.github.com>
2025-05-05 15:03:41 -0300

    Update pedidos.py

--- scr/entities/pedidos.py
+++ scr/entities/pedidos.py
@@ -1,8 +1,16 @@
 
 class Pedido:
+
+  next_codigo = 1
+  
   def __init__(self, cliente: "Cliente", maquina: "Maquina", fecha_recibido: datetime, fecha_entregado: datetime, estado: str):
+
+    self.id = Pedido._next_id 
+    Pedido._next_id += 1      
+    
     self.cliente = cliente
     self.maquina = maquina
     self.fecha_recibido = fecha_recibido
     self.fecha_entregado = fecha_entregado
     self.estado = estado
+    
