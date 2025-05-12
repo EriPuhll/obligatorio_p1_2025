05f980a9a6d0b8eee396c5fba9558d9a5ec0361b
eri_eri <146786312+EriPuhll@users.noreply.github.com>
2025-05-12 13:29:04 -0300

    Update reposiciones.py

--- scr/entities/reposiciones.py
+++ scr/entities/reposiciones.py
@@ -1,1 +1,15 @@
+class Reposicion:
+    # Contador de clase para ids de reposición
+    _next_id: int = 1
 
+    def __init__(self, pieza: Pieza, lotes: int, fecha_reposicion: datetime | None = None
+                ):
+        # Asigno el siguiente ID disponible
+                  
+        self.id = Reposicion._next_id       # ID único de la reposición
+                  
+        Reposicion._next_id += 1            # Incremento el contador de IDs
+
+        self.pieza = pieza                  # Pieza que se repone
+        self.lotes = lotes                  # Cantidad de lotes adquiridos
+        self.fecha_reposicion = fecha_reposicion or datetime.now() # Fecha de la reposición
