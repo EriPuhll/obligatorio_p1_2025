b09aebf341d96eca21077ee31631993a2c624a1e
eri_eri <146786312+EriPuhll@users.noreply.github.com>
2025-05-12 13:24:31 -0300

    Update pedidos.py

--- scr/entities/pedidos.py
+++ scr/entities/pedidos.py
@@ -6,14 +6,18 @@ class Pedido:
 
   _next_id = 1
   
-  def __init__(self, cliente: Cliente, maquina: Maquina, fecha_recibido: datetime, fecha_entregado: datetime, estado: str):
+  def __init__(self, cliente: Cliente, maquina: Maquina, fecha_recibido: datetime, fecha_entregado: datetime | None = None, estado: str = "pendiente"):
 
-    self.id = Pedido._next_id 
-    Pedido._next_id += 1      
+    self.id = Pedido._next_id #ID único del pedido
+    Pedido._next_id += 1 #Incremento el contador de IDs      
+
+    #atributos de asociación
     
     self.cliente = cliente
     self.maquina = maquina
+
+    #Inicializo los atributos de tiempo y estado
+    
     self.fecha_recibido = fecha_recibido
     self.fecha_entregado = fecha_entregado
     self.estado = estado
-    
