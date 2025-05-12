d82b67692ea8e0d7524a0f0e1105772a6c725a8a
eri_eri <146786312+EriPuhll@users.noreply.github.com>
2025-05-12 11:45:46 -0300

    Update piezas.py

--- scr/entities/piezas.py
+++ scr/entities/piezas.py
@@ -1,9 +1,19 @@
 
 class Pieza: 
-  def __init__(self, codigo: int, descripcion: str, costo: float, lote: int, cantidad: int, reposicon):
-    self.codigo = codigo
-    self.descripcion = descripcion
-    self.costo = costo
-    self.lote = lote
-    self.cantidad = cantidad
-    #Ver que hacer con reposición
+
+  # Contador de clase para generar códigos únicos
+  _next_id: int = 1
+  
+  def __init__(self, descripcion: str, costo: float, tam_lote: int, stock_iniical: int):
+
+  # Asigno el siguiente ID disponible
+    
+        self.codigo = Pieza._next_id  # Pieza._next_id contiene el siguiente valor de id
+        Pieza._next_id += 1  # incremento el contador para la próxima instancia
+
+  # Inicializo los atributos de instancia
+    
+        self.descripcion = descripcion
+        self.costo = costo
+        self.tam_lote = tam_lote
+        self.stock = stock_inicial
