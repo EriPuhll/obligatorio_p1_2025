ef926eecc79a9176d289531d9bb0ce836d859d66
eri_eri <146786312+EriPuhll@users.noreply.github.com>
2025-05-19 13:01:35 -0300

    Update Requerimiento.py

--- entities/Requerimiento.py
+++ entities/Requerimiento.py
@@ -1,9 +1,11 @@
 from entities.Pieza import Pieza  #importamos clase
+from entities.Maquina import Maquina
 
 class Requerimiento:
     
-    def __init__(self, pieza: "Pieza", cantidad: int):
+    def __init__(self, maquina: "Maquina", pieza: "Pieza", cantidad: int):
 
         #atributos de instancia
+        self.maquina = maquina 
         self.pieza = pieza
         self.cantidad = cantidad
