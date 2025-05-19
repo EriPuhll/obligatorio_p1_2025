63157343db3711d5f1e939f12600deec91b46430
eri_eri <146786312+EriPuhll@users.noreply.github.com>
2025-05-19 12:23:44 -0300

    Update Pieza.py

--- entities/Pieza.py
+++ entities/Pieza.py
@@ -1,12 +1,14 @@
 class Pieza:
-    _next_id: int = 1
+    
+    _next_id: int = 1 #contador de id unico cada vez que se cree una pieza, atributo de clase
 
     def __init__(self, descripcion: str, costo: float, tam_lote: int, stock_inicial: int = 0): 
       
-      self.id = Pieza._next_id
-      Pieza._next_id += 1
+        self.id = Pieza._next_id #asigno id
+        Pieza._next_id += 1    #incremento id para la proxima pieza 
 
-      self.descripcion = descripcion
-      self.costo = costo
-      self.tam_lote = tam_lote
-      self.stock = stock_inicial
+        #atributos de instancia
+        self.descripcion = descripcion
+        self.costo = costo
+        self.tam_lote = tam_lote
+        self.stock = stock_inicial
